e141d10ac3a86ac1e94e9d636601b5271a86aa32
shocktherap <moccaify@gmail.com>
2014-06-01 23:40:10 +0700

    sidebar helper using span

--- app/helpers/sidebar_helper.rb
+++ app/helpers/sidebar_helper.rb
@@ -3,7 +3,8 @@ module SidebarHelper
     [
       { controller: 'home', icon: 'display', title: 'Dashboard', url: 'root_path' } ,
       { controller: 'companies', icon: 'sheriffs_star', title: 'Perusahaan', url: 'new_company_path' },
-      { controller: 'customers', icon: 'parents', title: 'Pelanggan', url: 'customers_path' }
+      { controller: 'customers', icon: 'parents', title: 'Pelanggan', url: 'customers_path' },
+      { controller: 'billings', icon: 'credit_card', title: 'Invoice', url: 'billings_path' }
     ]
   end
 
